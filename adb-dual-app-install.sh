a6eda9bb7778960b6effdf1a6ed0c81b62f9893c
creator54 <hi.creator54@gmail.com>
2023-07-19 00:56:47 +0530

    refactoring

--- adb-dual-app-install.sh
+++ adb-dual-app-install.sh
@@ -17,24 +17,26 @@ else
 		## Copy the split APKs from user 0 to the device
 		adb shell cp -r "$pkg_path/*.apk" "/data/local/tmp/$pkg_name/"
 
+		pkg_path="/data/local/tmp/$pkg_name"
+
 		## Perform the installation for user 95
 		TOTAL_APKS_SIZE=0
-		for x in $(adb shell "ls /data/local/tmp/$pkg_name/"); do
-			TOTAL_APKS_SIZE=$((TOTAL_APKS_SIZE + $(adb shell "ls -l /data/local/tmp/$pkg_name/$x" | awk '{print $5}')))
+		for x in $(adb shell "ls $pkg_path"); do
+			TOTAL_APKS_SIZE=$((TOTAL_APKS_SIZE + $(adb shell "ls -l $pkg_path/$x" | awk '{print $5}')))
 		done
 		echo $TOTAL_APKS_SIZE
 		SESSION_ID=$(adb shell pm install-create --user 95 -S $TOTAL_APKS_SIZE | cut -d "[" -f2 | cut -d "]" -f1)
 		echo "Session ID: $SESSION_ID"
 
 		## Get a list of all the APKs in the package directory
-		apk_list=$(adb shell "cd /data/local/tmp/$pkg_name; ls")
+		apk_list=$(adb shell "cd $pkg_path; ls")
 
 		## Loop through each APK and stage it for installation
 		index=0
 		for apk in $apk_list; do
-			apk_size=$(adb shell stat -c %s "/data/local/tmp/$pkg_name/$apk")
+			apk_size=$(adb shell stat -c %s "$pkg_path/$apk")
 			echo "Staging APK: $apk (size: $apk_size)"
-			adb shell pm install-write -S "$apk_size" "$SESSION_ID" "$index" "/data/local/tmp/$pkg_name/$apk"
+			adb shell pm install-write -S "$apk_size" "$SESSION_ID" "$index" "$pkg_path/$apk"
 			index=$((index + 1))
 		done
 
@@ -45,7 +47,7 @@ else
 			echo "Installation committed successfully"
 
 			## Remove the APKs from the temporary directory on the device
-			adb shell rm -rf "/data/local/tmp/$pkg_name"
+			adb shell rm -rf "$pkg_path"
 		else
 			echo "Failed to stage all APKs. Aborting installation."
 		fi
